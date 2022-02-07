038cb6bad0a15b44debad91f6ed697acd7d940ed
Andrei <andrei.stamate95@gmail.com>
2022-02-07 23:19:26 +0200

    [AS] Added problem 3

---
+++ src/test/kotlin/medium/s0003/LengthOfLongestSubstringTest.kt
@@ -0,0 +1,22 @@
+package medium.s0003
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class LengthOfLongestSubstringTest {
+
+    @Test
+    fun test1() {
+        assertEquals(3, LengthOfLongestSubstring.lengthOfLongestSubstring("abcabcbb"))
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(3, LengthOfLongestSubstring.lengthOfLongestSubstring("pwwkew"))
+    }
+
+    @Test
+    fun test3() {
+        assertEquals(3, LengthOfLongestSubstring.lengthOfLongestSubstring("dvdf"))
+    }
+}
