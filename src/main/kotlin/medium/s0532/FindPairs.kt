ea9dcc3b0e2236d83fc60bae808e481c049ae35a
Andrei <andrei.stamate95@gmail.com>
2022-02-10 01:30:33 +0200

    [AS] Added problem 532

---
+++ src/main/kotlin/medium/s0532/FindPairs.kt
@@ -0,0 +1,20 @@
+package medium.s0532
+
+class FindPairs {
+    companion object {
+        fun findPairs(nums: IntArray, k: Int): Int {
+            if (nums.isEmpty()) return 0
+
+            val numbers = mutableMapOf<Int, Boolean>()
+            for (index in nums.indices) {
+                numbers[nums[index]] = numbers[nums[index]] != null
+            }
+            if (k == 0)
+                return numbers.filter { it.value }.count()
+
+            return numbers
+                .filter { numbers[it.key - k] != null }
+                .count()
+        }
+    }
+}
---
+++ src/test/kotlin/medium/s0532/FindPairsTest.kt
@@ -0,0 +1,23 @@
+package medium.s0532
+
+import medium.s0532.FindPairs.Companion.findPairs
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class FindPairsTest{
+    @Test
+    fun test1(){
+        val nums = intArrayOf(3,1,4,1,5)
+        val k = 2
+        val result = 2
+        assertEquals(result,findPairs(nums,k))
+    }
+
+    @Test
+    fun test2(){
+        val nums = intArrayOf(1,3,1,5,4)
+        val k = 0
+        val result = 1
+        assertEquals(result,findPairs(nums,k))
+    }
+}
